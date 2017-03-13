791232bf4be04a8a4162850305a0ff6e286b68ea
Mary Dickson <mdickson@gmail.com>
2017-03-13 12:59:41 -0700

    add filter struct and mock data in filter index

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -12,9 +13,35 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/filters", FilterIndex)
+	router.HandleFunc("/filters/{filterId}", FilterShow)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintln(w, "Welcome!")
 }
+
+func FilterIndex(w http.ResponseWriter, r *http.Request) {
+	filters := Filters{
+		Filter{Name: "filter name"},
+		Filter{Name: "another filter"},
+	}
+
+	json.NewEncoder(w).Encode(filters)
+}
+
+func FilterShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	filterId := vars["filterId"]
+	fmt.Fprintln(w, "Filter show:", filterId)
+}
+
+type Filter struct {
+	Name         string
+	CreatedTime  time.Time
+	ModifiedTime time.Time
+}
+
+type Filters []Filter
